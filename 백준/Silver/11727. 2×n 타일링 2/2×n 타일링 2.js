729a6c5f8496461fc6e5b0191f71a7c772214a9e
Junho Cheong <121740394+jjunohj@users.noreply.github.com>
2023-09-20 13:05:19 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 188 ms, Memory: 9652 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -0,0 +1,15 @@
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let fs = require("fs");
+const input = fs.readFileSync(file).toString().trim();
+const n = parseInt(input);
+
+const memo = {
+  1: 1,
+  2: 3,
+};
+
+for (let i = 3; i <= n; i++) {
+  memo[i] = (memo[i - 1] + memo[i - 2] * 2) % 10007;
+}
+
+console.log(memo[n]);
