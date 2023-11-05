4e6b6c39d07554bdd9c20d5a81981bc2e91b28f9
Junho Cheong <121740394+jjunohj@users.noreply.github.com>
2023-11-05 23:57:03 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 224 ms, Memory: 125064 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    dp[i] = dp[i - 1] + arr[i - 1]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    print(dp[j] - dp[i - 1])
