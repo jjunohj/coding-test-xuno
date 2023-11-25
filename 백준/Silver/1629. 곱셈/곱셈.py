bba05fca7c2525d85e79f6d3ee4fd487d1acd5a3
Junho Cheong <121740394+jjunohj@users.noreply.github.com>
2023-11-26 00:29:47 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+
+def power(a, b):
+    if b == 1:
+        return a % c
+    else:
+        tmp = power(a, b//2)
+        if b % 2 == 0:
+            return tmp * tmp % c
+        else:
+            return tmp * tmp * a % c
+
+print(power(a, b))
